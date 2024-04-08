a3042cbbfda7d8cb2d382dae7638234f4a2a1688
CesarBravo99 <cibravo2@uc.cl>
2024-04-08 14:38:40 -0400

    fix(auto report): :bug: install bug

--- auto_report/install.sh
+++ auto_report/install.sh
@@ -5,6 +5,14 @@ BASE_PATH="$(realpath --relative-to="$(pwd)" "$( cd "$( dirname "${BASH_SOURCE[0
 sudo apt update
 sudo apt-get install texlive-lang-english -y
 sudo apt-get install texmaker -y
+sudo apt-get install texlive-fonts-extra -y
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+echo "deb http://miktex.org/download/ubuntu bionic universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+sudo apt-get update
+sudo apt-get install miktex
+
+
 sudo apt install python3 python3-dev python3-venv -y
 sudo apt-get install wget
 wget https://bootstrap.pypa.io/get-pip.py
