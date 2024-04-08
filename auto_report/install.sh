35ea230143998e27b11c0ffe0aae8e61bea9bd66
CesarBravo99 <cibravo2@uc.cl>
2024-04-08 12:52:12 -0400

    fix(auto report): :bug: pdflatex lib

--- auto_report/install.sh
+++ auto_report/install.sh
@@ -4,6 +4,7 @@ BASE_PATH="$(realpath --relative-to="$(pwd)" "$( cd "$( dirname "${BASH_SOURCE[0
 
 sudo apt update
 sudo apt-get install texlive-lang-english -y
+sudo apt-get install texmaker -y
 sudo apt install python3 python3-dev python3-venv -y
 sudo apt-get install wget
 wget https://bootstrap.pypa.io/get-pip.py
