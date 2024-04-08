2a27b66a201f4972cfe07154934e21bd5f97740f
CesarBravo99 <cibravo2@uc.cl>
2024-04-08 12:30:13 -0400

    feat(auto report): :bug: texlive lang

--- auto_report/install.sh
+++ auto_report/install.sh
@@ -3,7 +3,7 @@
 BASE_PATH="$(realpath --relative-to="$(pwd)" "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )")"
 
 sudo apt update
-sudo apt-get install texlive-full -y
+sudo apt-get install texlive-lang-english -y
 sudo apt install python3 python3-dev python3-venv -y
 sudo apt-get install wget
 wget https://bootstrap.pypa.io/get-pip.py
