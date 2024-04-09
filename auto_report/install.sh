0b6495a641661710c5bed4e8537026f399adfc8a
CesarBravo99 <cibravo2@uc.cl>
2024-04-08 22:22:24 -0400

    fix(auto report): :bug: Latex install

--- auto_report/install.sh
+++ auto_report/install.sh
@@ -6,6 +6,7 @@ sudo apt update
 sudo apt-get install texlive-latex-base -y
 sudo apt-get install texlive-latex-recommended -y
 sudo apt-get install texlive-fonts-recommended -y
+sudo apt-get install texlive-latex-extra -y
 sudo apt-get install texlive-bibtex-recommended -y
 sudo apt-get install texlive-lang-english -y
 sudo apt-get install texlive-lang-spanish -y
