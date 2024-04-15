29eac2c3ab7596fa93fb06db9a16375e0306e970
CesarBravo99 <cibravo2@uc.cl>
2024-04-15 15:59:03 -0400

    feat(easy label): :sparkles: Add proto implementation of EasyLabel

--- easy_label/easy_label/views/easy_label.py
+++ easy_label/easy_label/views/easy_label.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from django.shortcuts import render, redirect
 from django.conf import settings
@@ -9,15 +10,39 @@ from easy_label.src import DataLoader
 def easy_label(request, hash):
     data_loader = DataLoader(hash)
     data = data_loader.load_data()
-    center = 'AMPARO GRANDE'
+    center = 'CARMEN'
+    module = "0"
     modules = data['centers'][center]['modules']
-    print(modules)
+    filename = data['centers'][center]['filename']
+    jaulas = data['centers'][center]['jails'][module]
+    scope = str(data['centers'][center]['scopes'][module])
+    x_flip = int(data['centers'][center]['x_flip'][module])
+    y_flip = int(data['centers'][center]['y_flip'][module])
+    double = int(data['centers'][center]['double'][module])
+    
+    templates = {
+        "lobera_fondo": os.path.join(os.path.normpath('imgs/assets/background/'), f'{filename}_{module}.png'),
+        "lobera_lateral": os.path.join(os.path.normpath('imgs/assets/lateral/'), f'{filename}_{module}.png'),
+        "lobera_cabecera": os.path.join(os.path.normpath('imgs/assets/fixed/'), f'seawolf_head_{x_flip}{y_flip}{double}.png'),
+        "tensor": os.path.join(os.path.normpath('imgs/assets/tensor/'), f'{filename}_{module}.png')
+    }
+
+    mamparo_labels = ["L" + str(i+1) + " - " + "L" + str(i+2) if x_flip else "L" + str(i+2) + " - " + "L" + str(i+1) for i in range(0, jaulas-2, 2)]
+    if y_flip: mamparo_labels = list(reversed(mamparo_labels))
+    pecera_labels = [str(scope)[0] + "0" + str(i+1) if i < 9 else str(scope)[0] + str(i+1) for i in range(jaulas)]
+
     context = {
+        "hash": hash,
         "data": data,
+        "centers": json.dumps(data['centers']),
         "center": center,
-        "modules": { 
-            "value": modules,
-            "iterator": range(1, modules+1)
-        }
+        "modules": range(1, modules+1),
+        "filename": filename,
+        "templates": templates,
+        "mamparo_labels": mamparo_labels,
+        "pecera_labels": pecera_labels,
+        "x_flip": x_flip,
+        "y_flip": y_flip,
+        "double": double,
     }
     return render(request, 'easy-label.html', context=context)
