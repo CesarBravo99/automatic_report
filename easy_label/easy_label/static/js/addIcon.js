6e6d84cc7dcd2fab66df4419b6d7b32d63ee2311
CesarBravo99 <cibravo2@uc.cl>
2024-04-24 11:54:57 -0300

    feat(easy label): :art: Improve context menu scripting

--- easy_label/easy_label/static/js/addIcon.js
+++ easy_label/easy_label/static/js/addIcon.js
@@ -1,11 +1,26 @@
 
-function addIcon(icon){
+function refeshIcons(){
+    refreshTemplates()
+    refreshFrames()
 };
 
-function createIcon(emoji, placementData) {
+function refreshTemplates(){
+    for(i = 0; i < imgs_name.length; i++){
+        if (imageMetaData[imgs_name[i]]['icon']['icon'] != ''){
+            icon = createIcon(imgs_name[i])
+            imageMetaData[imgs_name[i]]['icon']['template'].appendChild(icon)
+        }
+    }
+}
+
+function refreshFrames(){
+
+}
+
+function createIcon(img_name) {
     const icon = document.createElement("span");
     icon.className = "icon-class";
-    icon.textContent = emoji;
+    icon.textContent = imageMetaData[img_name]['icon']['icon'];
     icon.style.position = "absolute";
     return icon;
 }
--- easy_label/easy_label/static/js/contextMenu.js
+++ easy_label/easy_label/static/js/contextMenu.js
@@ -17,90 +17,117 @@ document.addEventListener('DOMContentLoaded', function () {
             template.addEventListener("contextmenu", function (event) {
                 event.preventDefault();
                 if (document.getElementById('selected-image').src.split('/').pop() == 'favicon_cover.png') return;
-                imgCoords = getImgCoords(event, system, template);
-                contextMenu[system.id].style.left = `${event.pageX}px`;
-                contextMenu[system.id].style.top = `${event.pageY}px`;
-                contextMenu[system.id].style.display = 'block';
+                menuMetadata = getMenuMetadata(event, system, template);
+                showMenu(event, contextMenu[system.id])
             });
-            template.addEventListener("click", function (event) {
-                contextMenuLoberas.style.display = 'none'
-                contextMenuPeceras.style.display = 'none'
-                contextMenuTensores.style.display = 'none'
-            });
+            template.addEventListener("click", hideMenu)
         });
         document.querySelectorAll("[id=context-link-" + system.id + ']').forEach((option) => {
             option.addEventListener('click', function(event) {
-            const annText = this.innerText.slice(3);
-            const annIcon = this.querySelector("span[role='icon']").textContent;
-            var img_name = document.getElementById('selected-image').src.split('/').pop()
-            saveAnnotation(img_name, imgCoords, annText)
-            addIcon(annIcon)
-            contextMenu[system.id].style.display = 'none';
+            const type = this.querySelector("span[role='icon']").ariaLabel;
+            const icon = this.querySelector("span[role='icon']").textContent;
+            const img_name = document.getElementById('selected-image').src.split('/').pop();
+            alert(icon)
+            saveImgMetadata(img_name, menuMetadata, type);
+            saveIconMetadata(img_name, menuMetadata, icon);
+            refeshIcons();
+            hideMenu();
             });
-            option.addEventListener('dblclick', function(event) {
-                event.preventDefault();
-            });
+            option.addEventListener('dblclick', function(event) {event.preventDefault()});
         });
     });
+});
 
-    function getIconCoords(event, system, template){
-        const imgRect = template.getBoundingClientRect();
-        var x = event.clientX - imgRect.left;
-        var y = event.clientY - imgRect.top;
+function getMenuMetadata(event, system, template) {
+    const imgRect = template.getBoundingClientRect();
+    const x = event.clientX - imgRect.left;
+    const y = event.clientY - imgRect.top;
+    var separator = '';
+    var jail = '';
 
-        return {
-            
+    if (system.id == 'lobera'){ 
+        sistema = 'lobero';
+        if (template.role == 'separator'){ separator = document.getElementById("mamparos-dropdown").value.split(' - ') } 
+        else { separator = 'None' };
+    } else if (system.id == 'pecera') { 
+        sistema = 'pecero' 
+        jail = 'Pecera ' + document.getElementById("peceras-dropdown").value;
+    } else { 
+        sistema = 'tensores' 
+    };
+
+    x_json = weights[system.id][template.role]['x'][0] + x*weights[system.id][template.role]['x'][1]
+    y_json = weights[system.id][template.role]['y'][0] + x*weights[system.id][template.role]['y'][1]
+
+
+    return {
+        'x': x,
+        'y': y,
+        'x_json': x_json,
+        'y_json': y_json,
+        'system': sistema,
+        'template': template,
+        'separator': separator,
+        'jail': jail,
+    };
+};
+
+function showMenu(event, menu){
+    menu.style.left = `${event.pageX}px`;
+    menu.style.top = `${event.pageY}px`;
+    menu.style.display = 'block';
+}
+
+function hideMenu(){
+    contextMenuLoberas.style.display = 'none'
+    contextMenuPeceras.style.display = 'none'
+    contextMenuTensores.style.display = 'none'
+}
+
+function saveImgMetadata(img_name, menuMetadata, type){
+    imageMetaData[img_name]['json']['module'] = (document.getElementById("modules-dropdown").value - 1).toString();
+    imageMetaData[img_name]['json']['system'] = menuMetadata.system;
+    imageMetaData[img_name]['json']['type'] = type;
+    imageMetaData[img_name]['json']['x'] = menuMetadata.x_json;
+    imageMetaData[img_name]['json']['y'] = menuMetadata.y_json;
+    if (menuMetadata.system == 'lobera'){ imageMetaData[img_name]['json']['separator'] = menuMetadata.separator}
+    else if (menuMetadata.system == 'pecera') { imageMetaData[img_name]['json']['jail'] = menuMetadata.jail };
+}
+
+function saveIconMetadata(img_name, menuMetadata, icon){
+    imageMetaData[img_name]['icon']['module'] = (document.getElementById("modules-dropdown").value - 1).toString();
+    imageMetaData[img_name]['icon']['system'] = menuMetadata.system;
+    imageMetaData[img_name]['icon']['template'] = menuMetadata.template;
+    imageMetaData[img_name]['icon']['icon'] = icon;
+    imageMetaData[img_name]['icon']['x'] = menuMetadata.x;
+    imageMetaData[img_name]['icon']['y'] = menuMetadata.y;
+}
+
+function refeshIcons(){
+    // alert(imageMetaData['0.JPG']['json']['y'])
+    refreshTemplates();
+    // refreshFrames()
+};
+
+function refreshTemplates(){
+    for(i = 0; i < imgs_name.length; i++){
+        if (imageMetaData[imgs_name[i]]['icon']['icon'] != ''){
+            icon = createIcon(imgs_name[i])
+            alert(icon.textContent)
+            // imageMetaData[imgs_name[i]]['icon']['template'].appendChild(icon)
         }
-    };
+    }
+}
 
-    function getImgCoords(event, system, template) {
-        const imgRect = template.getBoundingClientRect();
-        var x = event.clientX - imgRect.left;
-        var y = event.clientY - imgRect.top;
-        var separator = '';
-        var jail = '';
+function refreshFrames(){
 
-        if (system.id == 'lobera'){ 
-            sistema = 'lobero';
-            if (template.role == 'separator'){ separator = document.getElementById("mamparos-dropdown").value.split(' - ') } 
-            else { separator = 'None' };
-        } else if (system.id == 'pecera') { 
-            sistema = 'pecero' 
-            jail = 'Pecera ' + document.getElementById("peceras-dropdown").value;
-        } else { 
-            sistema = 'tensores' 
-        };
+}
 
-        x = weights[system.id][template.role]['x'][0] + x*weights[system.id][template.role]['x'][1]
-        y = weights[system.id][template.role]['y'][0] + x*weights[system.id][template.role]['y'][1]
-
-
-        return {
-            "x": x,
-            "y": y,
-            "system": sistema,
-            "separator": separator,
-            "jail": jail,
-        };
-    };
-
-    function saveAnnotation(img_name, coords, ann){
-        imageMetaData[img_name]['module'] = (document.getElementById("modules-dropdown").value - 1).toString();
-        imageMetaData[img_name]['system'] = coords.system
-        imageMetaData[img_name]['type'] = getType(ann)
-        imageMetaData[img_name]['x'] = coords.x
-        imageMetaData[img_name]['y'] = coords.y
-        if (coords.system == 'lobera'){ imageMetaData[img_name]['separator'] = coords.separator } 
-        else if (coords.system == 'pecera') { imageMetaData[img_name]['jail'] = coords.jail }
-    };
-
-    function getType(ann){
-        if (ann == 'Correcto'){ return 'correct'}
-        else if (ann == 'Rotura/Falla'){ return 'tear'}
-        else if (ann == 'Anomalía'){ return 'anomaly'}
-        else if (ann == 'Adherencia'){ return 'adherence'}
-        else if (ann == 'Mortalidad'){ return 'mortality'}
-        else if (ann == 'Falta tensión'){ return 'lack_tension'}
-        else if (ann == 'Sin tensión'){ return 'no_tension'}
-    };
-})
+function createIcon(img_name) {
+    const icon = document.createElement("span");
+    icon.className = "icon-class";
+    icon.textContent = imageMetaData[img_name]['icon']['icon'];
+    alert(imageMetaData[img_name]['icon']['icon'])
+    icon.style.position = "absolute";
+    return icon;
+};
