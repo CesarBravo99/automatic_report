d313179669dc21fa265a0655a54c2adc6aee6d20
CesarBravo99 <cibravo2@uc.cl>
2024-05-13 19:41:19 -0400

    fix(auto report): :bug: Fix bash bugs with directories in auto report proyect

--- auto_report/auto_report.sh
+++ auto_report/auto_report.sh
@@ -5,11 +5,14 @@ DATA_INPUT_DIR=$2
 DATA_OUTPUT_DIR=$3
 PDF_CONFIG=$4
 
-mkdir $DATA_EXPORT_DIR
-docker exec auto_report ./make_report.sh $DATA_INPUT_DIR $PDF_CONFIG
-docker cp $CONTAINER:temp/export/. $DATA_OUTPUT_DIR
+mkdir "$DATA_OUTPUT_DIR"
+docker exec $CONTAINER rm -r docker/import/.
+docker cp "$DATA_INPUT_DIR" $CONTAINER:docker/import/. 
+docker exec $CONTAINER ./make_report.sh $PDF_CONFIG
+docker cp $CONTAINER:docker/export/. "$DATA_OUTPUT_DIR"
+docker exec $CONTAINER rm -r docker/export/.
 
 echo "PDF Generated!"
 echo "You can find the PDF at $DATA_OUTPUT_DIR"
 
-# ./auto_report.sh auto_report test/input test/output a
+# ./auto_report.sh auto_report "/mnt/c/Users/jacmo/OneDrive - uc.cl/Escritorio/Tri-Chile/automatic_report/auto_report/test/input/." "/mnt/c/Users/jacmo/OneDrive - uc.cl/Escritorio/Tri-Chile/automatic_report/auto_report/output/" "a"
--- auto_report/make_report.sh
+++ auto_report/make_report.sh
@@ -1,38 +1,35 @@
 #!/bin/bash
 
-DATA_INPUT_DIR=$1
+# DATA_INPUT_DIR=$1
 # PDF_EXPORT_DIR=$2
-PDF_CONFIG=$2
+PDF_CONFIG=$1
 
 BASE_PATH="$(realpath --relative-to="$(pwd)" "$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )")"
 MAIN_PY_PATH="__main__.py"
-HASH_GENERATOR_PATH="src/utils/hash_generator.py"
-HASH=$(python3 "$HASH_GENERATOR_PATH")
-DATA_IMPORT_DIR="temp/import/$HASH/"
-DATA_EXPORT_DIR="temp/export/"
+# HASH_GENERATOR_PATH="src/utils/hash_generator.py"
+# HASH=$(python3 "$HASH_GENERATOR_PATH")
+HASH="pdf"
+DATA_IMPORT_DIR="docker/import/"
+DATA_EXPORT_DIR="docker/export/"
 # PDF_EXPORT_DIR="$BASE_PATH/content/export/$HASH/"
 
-mkdir $DATA_IMPORT_DIR
+# mkdir $DATA_IMPORT_DIR
 # mkdir $DATA_EXPORT_DIR
 # mkdir $PDF_EXPORT_DIR
 
-if [ -d "$DATA_INPUT_DIR" ]; then
-    cp -R $DATA_INPUT_DIR/* $DATA_IMPORT_DIR
-    ZIP_FILE="$(find $DATA_IMPORT_DIR -iname \*.zip)"
-    unzip $DATA_IMPORT_DIR/easylabel.zip -d $DATA_IMPORT_DIR
-    echo "The input folder has been copied successfully."
-    echo "Running the script with the following arguments:"
-    echo "Form Hash: $HASH"
-    echo "data_input_dir: $DATA_INPUT_DIR"
-    echo "report_output_path: $PDF_EXPORT_DIR"
-    python3 "$MAIN_PY_PATH" "--root=$BASE_PATH" "--imported_data_dir=$DATA_IMPORT_DIR" "--exported_data_dir=$DATA_EXPORT_DIR" "--pdf_config=$PDF_CONFIG" "--hash=$HASH"
+# if [ -d "$DATA_INPUT_DIR" ]; then
+# cp -R $DATA_INPUT_DIR/* $DATA_IMPORT_DIR
+# ZIP_FILE="$(find $DATA_IMPORT_DIR -iname \*.zip)"
+unzip $DATA_IMPORT_DIR/easylabel.zip -d $DATA_IMPORT_DIR
+echo "The input folder has been copied successfully."
+echo "Running the script with the following arguments:"
+# echo "Form Hash: $HASH"
+# echo "data_input_dir: $DATA_INPUT_DIR"
+# echo "report_output_path: $PDF_EXPORT_DIR"
+python3 "$MAIN_PY_PATH" "--root=$BASE_PATH" "--imported_data_dir=$DATA_IMPORT_DIR" "--exported_data_dir=$DATA_EXPORT_DIR" "--pdf_config=$PDF_CONFIG" "--hash=$HASH"
 
-    # sudo docker cp -R auto_report:$PDF_EXPORT_DIR/* $PDF_EXPORT_DIR
-    echo "Removing the temporary data"
-    rm -rf $DATA_IMPORT_DIR
-    # rm -rf $DATA_EXPORT_DIR
+# sudo docker cp -R auto_report:$PDF_EXPORT_DIR/* $PDF_EXPORT_DIR
+# echo "Removing the temporary data"
+# rm -rf $DATA_IMPORT_DIR
+# rm -rf $DATA_EXPORT_DIR
 
-
-else
-    echo "The source folder does not exist."
-fi
